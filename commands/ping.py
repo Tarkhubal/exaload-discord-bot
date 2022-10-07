596223fd7b6f5dbc066afe3936a68ee2caecf0a3
The_WildThomas <thb5309@gmail.com>
2022-10-07 13:17:28 +0200

    Ajout de l'embed et ajout de fonctions sur ping.py
    Ajout de trois fonctions :
    Latence
    Nombre de commandes effectuées
    Nombre de messages lus

--- commands/ping.py
+++ commands/ping.py
@@ -1,2 +1,24 @@
 def ping_msg():
     return "Pong ! :3"
+
+def ping_embed(used_commands, total_messages_read):
+    import discord
+    ping_embed = discord.Embed (
+        title       = "Pong ! :3",
+        description = "Détermine le ping du bot !",
+        color       = 0x00ff00
+        )
+    ping_embed.add_field (
+        name   = "Latence",
+        value  = str(round(client.latency * 1000)),
+        inline = False
+        )
+    ping_embed.add_field (
+        name   = "Nombre de commandes effectuées :",
+        value  = str(used_commands)
+    )
+    ping_embed.add_field (
+        name   = "Nombre de messages lus :",
+        value  = str(total_messages_read)
+    )
+    return ping_embed
--- main.py
+++ main.py
@@ -7,6 +7,10 @@ from random import *
 from all_commands import *
 from discord.utils import *
 import os
+from json import *
+
+used_commands  = 0
+total_messages_read = 0
 
 class MyClient(discord.Client):
     async def on_ready(self):
@@ -14,28 +18,40 @@ class MyClient(discord.Client):
 
     async def on_message(self, message):
         print(f'{message.author}: {message.content}')
+        total_messages_read += 1
         
         # don't respond to ourselves
         if message.author == self.user:
             return
 
         if 'ping' in message.content:
-            await message.channel.send(ping_msg())
+            used_commands += 1
+            await message.channel.send(embed = ping_embed(used_commands, total_messages_read))
             
         if 'hello' in message.content:
             await message.channel.send(hello_msg())
+            used_commands += 1
             
         if 'bye' in message.content:
             await message.channel.send(bye_msg())
+            used_commands += 1
 
         if 'help' in message.content:
             await message.channel.send(embed = help_msg())
+            used_commands += 1
             
         if 'tg' in message.content:
             await message.channel.send(tg_msg())
+            used_commands += 1
         
         if 'website' in message.content:
             await message.channel.send(embed = website_embed())
+            used_commands += 1
+        
+        if 'random' in message.content:
+            # to create
+            await message.channel.send("Une commande à venir !!")
+            
 
 
 intents = discord.Intents.default()
